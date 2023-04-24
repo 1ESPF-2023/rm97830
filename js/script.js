00fe12c1ffe8cbad17763b2af1bd42529518b1e5
filipesxr <rm97830@fiap.com.br>
2023-04-24 10:04:36 -0300

    Ajustes

--- js/script.js
+++ js/script.js
@@ -1,28 +1,48 @@
-//Recuperando o botão com o getElementById
-const botao = document.getElementById("btn-1");
+// Recuperando o botão com o getElementById
+//const botao = document.getElementById("btn-1");
 
-//Recuperar um item especifico com a
-//função querySelector(parâmetro)
-// const seletor = document.querySelector("li")
-// console.log(seletor.textContent)
+// Recuperar um item especifico com a
+// Função querySelector(parâmetro)
+//const seletor = document.querySelector("li")
+//console.log(seletor.textContent)
 
-//Recuerar uma lista de elementos com a função
+// Recuerar uma lista de elementos com a função
 //querySelecorAll
-const seletores = document.querySelectorAll("li");
+//const seletores = document.querySelectorAll("li");
  
-seletores.forEach((item)=>{
+//seletores.forEach((item)=>{
 
-    if (item.textContent == "Item-15") {
-        let meuItem = item;
-        console.log(`Item selecionado : ${meuItem.textContent}`);
-        meuItem.textContent = "TÁ DOMINADO!!"   
-    }
-})
+//    if (item.textContent == "Item-15") {
+//        let meuItem = item;
+//        console.log(`Item selecionado : ${meuItem.textContent}`);
+//        meuItem.textContent = "TÁ DOMINADO!!"   
+//    }
+//})
 
-//Para estudar
+// Para estudar
 //setTimeOut
 //setInterval
 //Random
 //Math.Ceil
 //Math.Random
 //Math.Floor
+
+let tmp = "";
+
+function mudarCor() {
+    let r = 0;
+    let g = 0;
+    let b = 0;
+    
+    r = Math.round(Math.random()*255);
+    g = Math.round(Math.random()*255);
+    b = Math.round(Math.random()*255);
+    
+    const cabecalho = document.querySelector(".cabecalho");
+    cabecalho.style.backgroundColor = `rgb(${r},${g},${b})`;
+
+    tmp = setTimeout(mudarCor, 5000);
+
+}
+
+mudarCor();
