2882ad89aac9a1995d0a138c7459154335923f90
filipesxr <rm97830@fiap.com.br>
2023-05-08 10:11:39 -0300

    Alterações

--- js/script.js
+++ js/script.js
@@ -23,26 +23,92 @@
 //setTimeOut
 //setInterval
 //Random
-//Math.Ceil
-//Math.Random
-//Math.Floor
+//Math.Ceil arredonda os números pra cima
+//Math.Random gera um número aleatório entre 0 e 1
+//Math.Floor arredonda os números pra baixo
+//Math.Round arredonda os números aleatoriamente
 
-let tmp = "";
+//let tmp = "";
 
-function mudarCor() {
-    let r = 0;
-    let g = 0;
-    let b = 0;
+//function mudarCor() {
+
+//    let r = 0;
+//    let g = 0;
+//    let b = 0;
     
-    r = Math.round(Math.random()*255);
-    g = Math.round(Math.random()*255);
-    b = Math.round(Math.random()*255);
+//    r = Math.round(Math.random() * 255);
+//    g = Math.round(Math.random() * 255);
+//    b = Math.round(Math.random() * 255);
     
-    const cabecalho = document.querySelector(".cabecalho");
-    cabecalho.style.backgroundColor = `rgb(${r},${g},${b})`;
+//    const cabecalho = document.querySelector(".cabecalho");
+//    cabecalho.style.backgroundColor = `rgb(${r},${g},${b})`;
 
-    tmp = setTimeout(mudarCor, 5000);
+//    tmp = setTimeout(mudarCor, 1000);
 
-}
+// }
 
-mudarCor();
+//mudarCor();
+
+// Maneira mais longa de fazer:
+//function mudaBanner() {
+
+//    const imgElement1 = document.querySelector(".banner-1 img");
+//    let num = Math.ceil(Math.random() * 3);
+//    imgElement1.src = "./img/banner-lateral-"+num+".png";
+//    setTimeout(mudaBanner2, 2000);
+
+//}
+
+//function mudaBanner2() {
+
+//    const imgElement2 = document.querySelector(".banner-2 img");
+//    let num = Math.ceil(Math.random() * 3);
+//    imgElement2.src = "./img/banner-lateral-"+num+".png";
+//    setTimeout(mudaBanner, 2000);
+
+//}
+
+//mudaBanner();
+
+// Fazendo em apenas uma função
+//function mudaBanner() {
+
+//    const imgElement1 = document.querySelector(".banner-1 img");
+//    let num = Math.ceil(Math.random() * 3);
+//    imgElement1.src = "./img/banner-lateral-"+num+".png";
+
+//    const imgElement2 = document.querySelector(".banner-2 img");
+//    num = Math.ceil(Math.random() * 3);
+//    imgElement2.src = "./img/banner-lateral-"+num+".png";
+
+//    setTimeout(mudaBanner, 2000);
+
+//}
+
+//mudaBanner();
+
+const botao = document.querySelector("button");
+
+botao.addEventListener("click", function(){
+    
+    if(this.textContent = "LIGAR"){
+        const imgElement = document.querySelector("img[alt='Luz Desligada']");
+        imgElement.src = "./img/pic_bulbon.gif";
+        imgElement.alt = "Luz Ligada";
+        this.textContent = "DESLIGAR";
+    }else{
+        const imgElement = document.querySelector("img[alt='Luz Ligada']");
+        imgElement.src = "./img/pic_bulboff.gif";
+        imgElement.alt = "Luz Desligada";
+        this.textContent = "LIGAR";
+    }
+});
+
+// Estudar em casa querySelectorAll
+// Exercício:
+
+//const botoes = document.querySelectorAll("button");
+
+//botoes.forEach( (botao)=>{
+//    botao
+//} );
